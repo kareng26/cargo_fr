267a3e74cb1c22dd325af8940341798aa1e7e4b6
benito martinez <benito@karens-MacBook-Air.local>
2024-02-28 22:44:55 +0300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            external: ["echarts"],
-        },
-    },
     plugins: [react()],
     resolve: {
         alias: {
